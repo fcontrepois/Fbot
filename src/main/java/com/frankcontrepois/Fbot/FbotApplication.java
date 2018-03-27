febe1e8a5ff89b0bddf6e706fe5f6c5d9e367d46
fcontrepois <frank@coblan.it>
2018-03-27 13:38:39 +0000

    First commit from start.spring.io

---
+++ src/main/java/com/frankcontrepois/Fbot/FbotApplication.java
@@ -0,0 +1,12 @@
+package com.frankcontrepois.Fbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbotApplication.class, args);
+	}
+}
---
+++ src/test/java/com/frankcontrepois/Fbot/FbotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.frankcontrepois.Fbot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FbotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
